f4916d907307fa118b761e1129708b71e3c0f6ec
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-02-09 23:54:49 +0530

    [Saurabh]Add CheckingGamblerLoseOrWonTheStakeForADay

--- gambling.js
+++ gambling.js
@@ -3,22 +3,34 @@ var betMoney=1;
 
 class Utility
 {
-
+   
     playGame()
     {
-        console.log("We are good to start the Game");
+       
         var win=Math.floor(Math.random()*10 % 2);
         if(win == 1)
         {
             stake=stake+betMoney;
-            console.log(`You won and you stake is ${stake}`);
         }
         else{
             stake=stake-betMoney;
-             console.log(`Sorry!! you lost the gamble ${stake}`);   
         }
+        return win;
     }
 
+    resignDay()
+    {
+        console.log("\n We are good to start the Gamble ");
+        while(stake > 50 && stake < 150)
+        {
+            this.playGame();                 
+        }
+        if(stake==50){
+        console.log(`Sorry!!You lost the game and ${stake} stakes \n`); 
+        }else{
+        console.log(`Congratulations!! You won and your stakes  is ${stake} \n`);
+        }
+    }
 }
 
 module.exports=new Utility();
