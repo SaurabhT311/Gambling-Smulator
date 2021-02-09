0ceccb6d1fd1949d8344692ee7cd28ca676d372a
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-02-10 01:27:00 +0530

    [Saurabh]Add CheckingGamblerLoseOrWonTheStakeForADay

--- gambling.js
+++ gambling.js
@@ -1,36 +1,51 @@
-var stake=100;
-var betMoney=1;
+ var Promise = require('promise');
+var stake = 100;
+var betMoney = 1;
 
-class Utility
-{
-   
-    playGame()
-    {
-       
-        var win=Math.floor(Math.random()*10 % 2);
-        if(win == 1)
-        {
-            stake=stake+betMoney;
+class Utility {
+
+    playGame() {
+
+        var win = Math.floor(Math.random() * 10 % 2);
+        if (win == 1) {
+            stake = stake + betMoney;
         }
-        else{
-            stake=stake-betMoney;
+        else {
+            stake = stake - betMoney;
         }
         return win;
     }
 
-    resignDay()
-    {
-        console.log("\n We are good to start the Gamble ");
-        while(stake > 50 && stake < 150)
-        {
-            this.playGame();                 
-        }
-        if(stake==50){
-        console.log(`Sorry!!You lost the game and ${stake} stakes \n`); 
-        }else{
-        console.log(`Congratulations!! You won and your stakes  is ${stake} \n`);
-        }
+    stakesMatching() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (stake == 50) {
+                    let answer = console.log(`Sorry!! You lost the game and ${stake} stakes \n`);
+                    resolve(answer);
+                } else {
+                    let winner = console.log(`Congratulations!! You won and your stakes is ${stake} \n`);
+                    reject(winner);
+                }
+            }, 1000);
+        })
     }
-}
 
-module.exports=new Utility();
+    match() {
+        this.stakesMatching().then(resolved => {
+            console.log(resolved)
+        }).catch(rejected => {
+            console.log(rejected)
+        })
+    }
+
+    resignDay() {
+        var i = 0;
+        console.log("\n We are good to start the Gamble ");
+        while (stake > 50 && stake < 150) {
+            this.playGame();
+        }
+        this.stakesMatching();
+    }
+        }
+
+module.exports = new Utility();
