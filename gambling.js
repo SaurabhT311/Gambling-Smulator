dc0d793d4cc30fcf6d6210d029cc1e03bf2eb55b
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-02-09 20:54:03 +0530

    [Saurabh]Add WinOrLose

--- gambling.js
+++ gambling.js
@@ -6,14 +6,16 @@ class Utility
 
     playGame()
     {
+        console.log("We are good to start the Game");
         var win=Math.floor(Math.random()*10 % 2);
         if(win == 1)
         {
             stake=stake+betMoney;
+            console.log(`You won and you stake is ${stake}`);
         }
         else{
             stake=stake-betMoney;
-                
+             console.log(`Sorry!! you lost the gamble ${stake}`);   
         }
     }
 
